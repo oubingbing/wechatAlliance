54bacb188b619bb65404c3ba5bf04282756b4ae7
ouzhibing <ouzhibing@galaxymx.com>
2018-06-04 18:14:51 +0800

    阿里云对接图片比对接口

--- app/Http/Service/Http.php
+++ app/Http/Service/Http.php
@@ -6,7 +6,7 @@
  * Time: 上午11:55
  */
 
-namespace App\Http\Logic;
+namespace App\Http\Service;
 
 
 class Http
@@ -78,6 +78,105 @@ class Http
         curl_close($curl); // 关闭CURL会话
 
         return ['status_code' => $status, 'result' => json_decode($result, true)];
+    }
+
+    /**
+     * 调用阿里云人脸对比功能
+     *
+     * 1% - 10% 哈哈你们可以愉快的拍拖
+     *
+     * 10% - 50 夫妻情人
+     *
+     * 50% - 70% 家人
+     *
+     * 70% 以上的是本人
+     *
+     * @author yezi
+     *
+     * @param $image1
+     * @param $image2
+     * @param int $imageType
+     * @return mixed
+     */
+    public function compareFace($image1,$image2,$imageType=0)
+    {
+
+        /*array:6 [
+                "confidence" => 45.945251464844
+          "thresholds" => array:3 [
+                0 => 61.0
+            1 => 69.0
+            2 => 75.0
+          ]
+          "rectA" => array:4 [
+                0 => 115
+            1 => 222
+            2 => 382
+            3 => 550
+          ]
+          "rectB" => array:4 [
+                0 => 213
+            1 => 540
+            2 => 484
+            3 => 618  //如图片中没有人脸，返回矩形框数值均为0
+          ]
+          "errno" => 0 //返回正常
+          "request_id" => "406081ae-3a79-4562-a4db-6e7e0fc3b465"
+        ]*/
+
+        $akId = env('ALI_ID');
+        $akSecret = env('ALI_SECRET');
+        $url = env('ALI_URL');
+
+        $content = [
+            "type"=>$imageType,
+            "image_url_1"=>$image1,
+            "image_url_2"=>$image2
+        ];
+
+        $options = array(
+            'http' => array(
+                'header' => array(
+                    'accept'=> "application/json",
+                    'content-type'=> "application/json",
+                    'date'=> gmdate("D, d M Y H:i:s \G\M\T"),
+                    'authorization' => ''
+                ),
+                'method' => "POST",
+                'content' => json_encode($content) //如有数据，请用json_encode()进行编码
+            )
+        );
+        $http = $options['http'];
+        $header = $http['header'];
+        $urlObj = parse_url($url);
+        if(empty($urlObj["query"]))
+            $path = $urlObj["path"];
+        else
+            $path = $urlObj["path"]."?".$urlObj["query"];
+        $body = $http['content'];
+        if(empty($body))
+            $bodymd5 = $body;
+        else
+            $bodymd5 = base64_encode(md5($body,true));
+        $stringToSign = $http['method']."\n".$header['accept']."\n".$bodymd5."\n".$header['content-type']."\n".$header['date']."\n".$path;
+        $signature = base64_encode(
+            hash_hmac(
+                "sha1",
+                $stringToSign,
+                $akSecret, true));
+        $authHeader = "Dataplus "."$akId".":"."$signature";
+        $options['http']['header']['authorization'] = $authHeader;
+        $options['http']['header'] = implode(
+            array_map(
+                function($key, $val){
+                    return $key.":".$val."\r\n";
+                },
+                array_keys($options['http']['header']),
+                $options['http']['header']));
+        $context = stream_context_create($options);
+        $file = file_get_contents($url, false, $context );
+
+        return json_decode($file,true);
     }
 
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Unit;
 
-use App\Colleges;
-use App\User;
+use App\Http\Service\Http;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -22,11 +22,15 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 
-    /**
-     * @test
-     */
-    public function demo()
+
+    public function testDemo()
     {
 
+        $image1 = "http://image.kucaroom.com/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20180604170505.jpg";
+        $image2 = "http://image.kucaroom.com/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20180604155126.jpg";
+
+        $result = app(Http::class)->compareFace($image1,$image2,$imageType=0);
+
+        dd($result);
     }
 }
