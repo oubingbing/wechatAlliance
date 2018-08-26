115eb192920b43bb0e1b092c23fbf93ff50acfb4
bingbing <875307054@qq.com>
2018-08-26 23:00:02 +0800

    修改电信号码校验规则

--- app/common/helpers.php
+++ app/common/helpers.php
@@ -35,7 +35,7 @@ if( ! function_exists('webResponse') ){
  */
 if( ! function_exists('validMobile') ){
     function validMobile($mobile){
-        return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile);
+        return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[3,4,0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile);
     }
 }
 
