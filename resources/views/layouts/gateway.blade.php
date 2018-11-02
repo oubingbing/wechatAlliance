491f9ec12ab8e4ebb6e0127312c8aa83bdafdf14
ouzhibing <ouzhibing@galaxymx.com>
2018-11-02 15:05:22 +0800

    添加百度联盟验证码

--- resources/views/layouts/gateway.blade.php
+++ resources/views/layouts/gateway.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="baidu_union_verify" content="98d08a1e6a234cbf644d940d26fdf911">
 
     <title>校园小情书</title>
     <link rel="stylesheet" href="{{ asset('css/font.css') }}">
