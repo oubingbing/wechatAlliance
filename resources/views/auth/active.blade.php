40bfb78a55ac3562f8d5ff277639f0afee2bb147
ouzhibing <ouzhibing@galaxymx.com>
2018-12-29 17:36:24 +0800

    修复激活账号

--- resources/views/auth/active.blade.php
+++ resources/views/auth/active.blade.php
@@ -4,7 +4,7 @@
     <body class="login-bg">
         <div style="width: 50%;background: white;margin: 100px auto;border-radius: 20px">
             <h2 style="text-align: center;padding: 50px">
-                激活账号成功，快去<a style="color: blue" href="{{route('login')}}">登录</a>吧！<span id="time" style="color: red">1</span>
+                激活账号成功，快去<a style="color: blue" href="/login">登录</a>吧！<span id="time" style="color: red">1</span>
             </h2>
         </div>
     <script>
@@ -15,7 +15,7 @@
             console.log(time);
         },1000);
         setTimeout(function () {
-            window.location.href = "{{route('login')}}";
+            window.location.href = "/login";
         },5000)
     </script>
     </body>
