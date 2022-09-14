ec611d8d2040a68698b4e646bde11869e6ac5f61
ouzhibing <ouzhibing@galaxymx.com>
2022-09-14 15:25:51 +0800

    fix:删除卖舍友性别和专业字段

--- app/Http/Controllers/Wechat/SaleFriendController.php
+++ app/Http/Controllers/Wechat/SaleFriendController.php
@@ -48,19 +48,27 @@ class SaleFriendController extends Controller
 
         $rule = [
             'name'        => 'required',
-            'gender'      => 'required',
-            'major'       => 'required',
-            'expectation' => 'required',
+            //'gender'      => 'required',
+            //'major'       => 'required',
+            //'expectation' => 'required',
             'introduce'   => 'required'
         ];
 
         $messages = [
             'name.required'        => '名字不能为空',
-            'gender.required'      => '性别不能为空',
-            'major.required'       => '专业不能为空',
-            'Expectation.required' => '期望不能为空',
+            //'gender.required'      => '性别不能为空',
+            //'major.required'       => '专业不能为空',
+            //'Expectation.required' => '期望不能为空',
             'introduce.required'   => '介绍不能为空',
         ];
+
+        if(empty($gender)){
+            $gender = 1;
+        }
+
+        if(empty($major)){
+            $major = "无";
+        }
 
         $validator = \Validator::make(request()->input(), $rule,$messages);
         if ($validator->fails()) {
